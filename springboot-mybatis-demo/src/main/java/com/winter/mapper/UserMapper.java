752b73ba7a91d5a81bd8219a1798aeff84b972c3
Tom <18938961795@189.cn>
2018-05-29 23:06:50 +0800

    fix 2
    version 2, ready to add database operation

--- springboot-mybatis-demo/src/main/java/com/winter/mapper/UserMapper.java
+++ springboot-mybatis-demo/src/main/java/com/winter/mapper/UserMapper.java
@@ -1,9 +1,23 @@
 package com.winter.mapper;
 
 import com.winter.model.User;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
+@Mapper //一定要加这个mapper否则会报错，why，待查 https://www.cnblogs.com/JealousGirl/p/bean.html
 public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
     int insert(User record);
 
     int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+    //这个方式我自己加的
+    List<User> selectAllUser();
 }
