25d6b3d7f85759267e069ce6e9df11406f3d4333
Tom <18938961795@189.cn>
2018-05-29 21:57:59 +0800

    fixed
    the application class must be in the com.winter directory

--- springboot-mybatis-demo/src/main/java/com/winter/SpringbootMybatisDemoApplication.java
+++ springboot-mybatis-demo/src/main/java/com/winter/SpringbootMybatisDemoApplication.java
@@ -1,8 +1,7 @@
-package com.winter.springbootmybatisdemo;
+package com.winter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 
