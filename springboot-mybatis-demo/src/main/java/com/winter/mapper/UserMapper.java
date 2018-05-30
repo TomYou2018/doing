ea0f4a9c72c379ff1a9ed706f73cf93aeea5e123
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-05-30 09:08:52 +0800

    new generator
    generate a new table: from t_user to users

--- springboot-mybatis-demo/src/main/java/com/winter/mapper/UserMapper.java
+++ springboot-mybatis-demo/src/main/java/com/winter/mapper/UserMapper.java
@@ -5,19 +5,10 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper //一定要加这个mapper否则会报错，why，待查 https://www.cnblogs.com/JealousGirl/p/bean.html
+@Mapper
 public interface UserMapper {
-    int deleteByPrimaryKey(Integer userId);
-
     int insert(User record);
 
     int insertSelective(User record);
-
-    User selectByPrimaryKey(Integer userId);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
-    //这个方式我自己加的
     List<User> selectAllUser();
 }
--- springboot-mybatis-demo/src/main/java/com/winter/model/User.java
+++ springboot-mybatis-demo/src/main/java/com/winter/model/User.java
@@ -1,28 +1,26 @@
 package com.winter.model;
 
 public class User {
-    private Integer userId;
+    private Integer id;
 
-    private String userName;
+    private String name;
 
     private String password;
 
-    private String phone;
-
-    public Integer getUserId() {
-        return userId;
+    public Integer getId() {
+        return id;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getName() {
+        return name;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
     }
 
     public String getPassword() {
@@ -31,13 +29,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password == null ? null : password.trim();
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone == null ? null : phone.trim();
     }
 }
