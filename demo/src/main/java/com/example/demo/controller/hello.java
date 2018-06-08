d7a13fe299905ef2b70a9f23a350a3abe3f88d5c
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-08 17:38:03 +0800

    1

---
+++ demo/src/main/java/com/example/demo/controller/hello.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+@RestController
+public class hello {
+
+    @RequestMapping("/")
+    public String home() {
+
+        //List<ServiceInstance> instances = client.getInstances("hello-service");
+        return "Hello worldx";
+    }
+}
