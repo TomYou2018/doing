0e8908192dd01b6bb0aaa97dff8f5732e8b5d99b
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-08 15:55:14 +0800

    provider

---
+++ springcloudprovider/src/main/java/com/example/springcloudprovider/SpringcloudproviderApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudproviderApplication.class, args);
+    }
+}
