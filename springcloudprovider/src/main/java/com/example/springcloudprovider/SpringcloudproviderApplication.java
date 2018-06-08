19a6d502d979563ae1193d62348fabda9c316d9f
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-08 17:24:55 +0800

    2

--- springcloudprovider/src/main/java/com/example/springcloudprovider/SpringcloudproviderApplication.java
+++ springcloudprovider/src/main/java/com/example/springcloudprovider/SpringcloudproviderApplication.java
@@ -3,13 +3,13 @@ package com.example.springcloudprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SpringcloudproviderApplication {
 
     public static void main(String[] args) {
