74f49f43510e4aa2d29bffa7a72e7fa4b7d3d0e0
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-08 23:55:15 +0800

    11

--- springcloudprovider/src/main/java/com/example/springcloudprovider/controller/helloWorld.java
+++ springcloudprovider/src/main/java/com/example/springcloudprovider/controller/helloWorld.java
@@ -2,23 +2,32 @@ package com.example.springcloudprovider.controller;
 
 
 import javafx.application.Application;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import java.util.List;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 public class helloWorld {
 
-
-   private DiscoveryClient client;
+    private final Logger logger = LogManager.getLogger();
+    private DiscoveryClient client;
 
     @RequestMapping("/")
     public String home() {
 
         List<ServiceInstance> instances = client.getInstances("hello-service");
+        logger.info("test");
+        System.out.println("test");
+
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+
         return "Hello world2";
     }
 //    public static void main(String[] args) {
