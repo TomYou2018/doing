107ea8df836fa7a4452c1feec37ab0892b0c98ef
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-10 22:00:58 +0800

    two servers registe with each other
    just use port to direct to another server
    
    当有多个注册中心时，服务只要向一个注册中心注册就可以了。因为所有的注册中心都是彼此相连的。
    
    3. 代码配置
    
    3.1 开启两个Eureka server，分别在不同的端口（80，81）

--- SpringCloudServer/src/main/java/com/example/SpringCloudServer/SpringCloudServerApplication.java
+++ SpringCloudServer/src/main/java/com/example/SpringCloudServer/SpringCloudServerApplication.java
@@ -2,10 +2,14 @@ package com.example.SpringCloudServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
+
 public class SpringCloudServerApplication {
 
 	public static void main(String[] args) {
