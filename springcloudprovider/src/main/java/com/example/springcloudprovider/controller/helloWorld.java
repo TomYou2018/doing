ffb693d346c09d59ee48119bc5914246ff34d8b4
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-10 21:34:56 +0800

    done with multi service provider
    just run multi service provider with different port but same service name.

--- springcloudprovider/src/main/java/com/example/springcloudprovider/controller/helloWorld.java
+++ springcloudprovider/src/main/java/com/example/springcloudprovider/controller/helloWorld.java
@@ -3,6 +3,7 @@ package com.example.springcloudprovider.controller;
 
 import javafx.application.Application;
 import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,9 @@ public class helloWorld {
     private final Logger logger = LogManager.getLogger();
     private DiscoveryClient client;
 
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping("/")
     public String home() {
 
@@ -28,7 +32,7 @@ public class helloWorld {
 //            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
 //        }
 
-        return "Hello world2";
+        return "Hello world " + "from " + port;
     }
 //    public static void main(String[] args) {
 //        new SpringApplicationBuilder(Application.class).web(true).run(args);
